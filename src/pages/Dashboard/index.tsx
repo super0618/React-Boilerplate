e599fc92921e96f27e19fc3e284b678ae71fe5fd
Super <newestStar1990@gmail.com>
2024-07-22 11:51:45 -0400

    feat: added css module

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./styles.css";
+import styles from "./styles.module.css";
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>Dashboard</h1>
     </div>
   );
