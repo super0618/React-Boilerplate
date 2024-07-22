491d301bbd4fd7d0385dd8df5d6136c52858caf4
Super <newestStar1990@gmail.com>
2024-07-22 12:57:46 -0400

    feat: added routes
    Co-authored-by: Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
    Co-authored-by: Georgi Gerganov <1991296+ggerganov@users.noreply.github.com>
    Co-authored-by: Andrej <241138+karpathy@users.noreply.github.com>
    Co-authored-by: George Hotz <72895+geohot@users.noreply.github.com>
    Co-authored-by: slaren <2141330+slaren@users.noreply.github.com>
    Co-authored-by: Joel Yliluoma <39278+bisqwit@users.noreply.github.com>
    Co-authored-by: Andre Weissflog <1699414+floooh@users.noreply.github.com>
    Co-authored-by: Radoslav Gerganov <271616+rgerganov@users.noreply.github.com>
    Co-authored-by: Kovid Goyal <1308621+kovidgoyal@users.noreply.github.com>
    Co-authored-by: Brian Armstrong <15702338+brian-armstrong@users.noreply.github.com>
    Co-authored-by: Alex Teichman <1704431+teichman@users.noreply.github.com>
    Co-authored-by: Tristan Hume <887610+trishume@users.noreply.github.com>
    Co-authored-by: Erik Bjäreholt <1405370+ErikBjare@users.noreply.github.com>
    Co-authored-by: Johannes Schönberger <134307+ahojnnes@users.noreply.github.com>
    Co-authored-by: Pete Warden <161459+petewarden@users.noreply.github.com>
    Co-authored-by: Michael Nielsen <52171+mnielsen@users.noreply.github.com>
    Co-authored-by: jngiam <1147434+jngiam@users.noreply.github.com>
    Co-authored-by: Heather <140420+harthur@users.noreply.github.com>

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const About: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-const Dashboard: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Dashboard</h1>
+      <h1>Home</h1>
     </div>
   );
 };
 
-export default Dashboard;
+export default Home;
