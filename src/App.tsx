a439df866e1b6fd26db3c471c8e22912ed097e94
Super <newestStar1990@gmail.com>
2024-07-24 04:04:46 -0400

    fix: fix some configuration files
    Co-authored-by: DevSenior1990 <158775117+DevSenior1990@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useRoutes, Outlet } from "react-router-dom";
+import type { RouteObject } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 const App: React.FC = () => {
-  const routes = [
+  const routes: RouteObject[] = [
     {
       path: "/",
       element: <Outlet />,
