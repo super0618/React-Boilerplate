5779ef5454d487caf780ea1a1baceb34f907490e
Super <newestStar1990@gmail.com>
2024-07-25 02:45:00 -0400

    feat: added sass varialbes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles/styles.scss";
+import "./styles/globals.scss";
 
 const rootNode = document.getElementById("root");
 
